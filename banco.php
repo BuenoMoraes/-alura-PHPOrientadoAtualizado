13802a708533c462a500447bf91aba3d8272348e
Matheus Bueno <bueno_matheus@outlook.com>
2021-12-30 10:13:52 -0300

    Atuaização final

--- banco.php
+++ banco.php
@@ -2,10 +2,17 @@
 
 require_once 'src/Conta.php';
 
-$primeiraConta = new Conta();
-$primeiraConta->depositar(500);
-$primeiraConta->sacar(300); // isso é ok
-$primeiraConta->defineCpfTitular('123.456.789-10');
+$primeiraConta = new Conta('123.456.789-10', 'Vinicius Dias');
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300); // isso é ok
 
-echo $primeiraConta->recuperarSaldo().PHP_EOL;
-echo $primeiraConta->recuperarCpfTitular();
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+$segundaConta = new Conta('698.549.548-10', 'Patricia');
+var_dump($segundaConta);
+
+$outra = new Conta('123', 'Abcdefg');
+unset($segundaConta);
+echo Conta::recuperaNumeroDeContas();
--- src/conta.php
+++ src/conta.php
@@ -2,11 +2,27 @@
 
 class Conta
 {
-    private string $cpfTitular;
-    private string $nomeTitular;
-    private float $saldo = 0;
+    private $cpfTitular;
+    private $nomeTitular;
+    private $saldo;
+    private static $numeroDeContas = 0;
 
-    public function sacar(float $valorASacar): void
+    public function __construct(string $cpfTitular, string $nomeTitular)
+    {
+        $this->cpfTitular = $cpfTitular;
+        $this->validaNomeTitular($nomeTitular);
+        $this->nomeTitular = $nomeTitular;
+        $this->saldo = 0;
+
+        self::$numeroDeContas++;
+    }
+
+    public function __destruct()
+    {
+        self::$numeroDeContas--;
+    }
+
+    public function saca(float $valorASacar): void
     {
         if ($valorASacar > $this->saldo) {
             echo "Saldo indisponível";
@@ -16,7 +32,7 @@ class Conta
         $this->saldo -= $valorASacar;
     }
 
-    public function depositar(float $valorADepositar): void
+    public function deposita(float $valorADepositar): void
     {
         if ($valorADepositar < 0) {
             echo "Valor precisa ser positivo";
@@ -26,7 +42,7 @@ class Conta
         $this->saldo += $valorADepositar;
     }
 
-    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    public function transfere(float $valorATransferir, Conta $contaDestino): void
     {
         if ($valorATransferir > $this->saldo) {
             echo "Saldo indisponível";
@@ -37,28 +53,32 @@ class Conta
         $contaDestino->depositar($valorATransferir);
     }
 
-    public function recuperarSaldo(): float
+    public function recuperaSaldo(): float
     {
         return $this->saldo;
     }
 
-
-    public function defineCpfTitular(string $cpf): void
-    {
-        $this->cpfTitular = $cpf;
-    }
-
-    public function recuperarCpfTitular(): string
+    public function recuperaCpfTitular(): string
     {
         return $this->cpfTitular;
     }
 
-    public function definirNomeTitular(string $nome): void
-    {
-        $this->nomeTitular = $nome;
-    }
-    public function recuperarNomeTitular(): string
+    public function recuperaNomeTitular(): string
     {
         return $this->nomeTitular;
     }
+
+    private function validaNomeTitular(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+
+    public static function recuperaNumeroDeContas(): int
+    {
+        return self::$numeroDeContas;
+    }
 }
+
